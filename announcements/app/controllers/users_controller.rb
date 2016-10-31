a71ba8bfbefe4d8c2b327ee779dec58eabc52b04
Maxim Domentii <domentiimaxim@yahoo.com>
2016-11-01 00:09:03 +0200

    Basic operation on users

--- announcements/app/controllers/users_controller.rb
+++ announcements/app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -7,19 +11,40 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
-    byebug
     @user = User.new user_params
+
     if @user.save
-      # Handle a successful save.
+      redirect_to @user
     else
       render 'new'
     end
   end
 
+  def update
+    @user = User.find params[:id]
+
+    if @user.update user_params
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find params[:id]
+    @user.destroy
+
+    redirect_to users_path
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :firstname, :lastname, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :firstname, :lastname, :phone, :password, :password_confirmation)
   end
 end
