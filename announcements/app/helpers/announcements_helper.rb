75168354474881707cd17613791e235926586e4f
Maxim Domentii <domentiimaxim@yahoo.com>
2016-11-26 15:49:13 +0200

    Add recommendation functionalities on home page

--- announcements/app/helpers/announcements_helper.rb
+++ announcements/app/helpers/announcements_helper.rb
@@ -15,4 +15,22 @@ module AnnouncementsHelper
   def announcement_image_path imageId, annId
     UrlHelpers.announcements_images_path(:id => imageId, :announcement_id => annId)
   end
+
+  def get_most_viewed
+    all_ann = Announcement.all
+    viewed_ann = all_ann.select {|item| item.clicks > 0}
+    viewed_ann.sort_by {|item| -item.clicks}
+  end
+
+  def get_most_recent
+    all_ann = Announcement.all
+    ordered_ann = all_ann.sort {|item1, item2| item2.created_at <=> item1.created_at}
+  end
+
+  def get_best_rated
+    all_ann = Announcement.all
+    rated_ann = all_ann.select {|item| item.ratings.count > 0}
+    rated_ann.sort_by {|item| -(item.ratings.sum(:rank) / item.ratings.count)}
+  end
+
 end
